686b4a9d6f58dbd6eb1752952d368d374237ac32
Yahav <freeman.yav@gmail.com>
2022-02-28 16:07:23 +0200

    finishing css touches

--- weather/src/cmps/CityWeather.jsx
+++ weather/src/cmps/CityWeather.jsx
@@ -20,20 +20,24 @@ export default function CityWeather(weather) {
   );
 
   return (
-    <div className="weather-side middle">
-      <div className="city-header">
+    <div className="weather-side flex column auto-center">
+      <div className="city-header flex column auto-center">
         <p className="city-name">{weather.location.name}</p>
         <p className="city-country">{weather.location.country.toLowerCase()}</p>
         <p className="city-time">
           {day}/{month}/{weather.location.localtime.slice(2, 4)} at {hr}
         </p>
       </div>
-      <div className="city-temp-container flex auto-center">
+      <div className="city-temp-container flex column auto-center">
+        <div className="temp-celsius flex">
         <p className="city-temp">{Math.round(weather.current.temp_c)}</p>
         <div className="celsius"></div>
+        </div>
         <p className="city-condition">{weather.current.condition.text}</p>
       </div>
-      <div className="stats flex">
+      <div className="stats flex auto-center">
+        <div className="dividing-stats flex">
+
         <div>
           <p className="stat-name">precipitation</p>
           <p className="stat">{weather.current.precip_mm} mm</p>
@@ -42,21 +46,24 @@ export default function CityWeather(weather) {
           <p className="stat-name">humidity</p>
           <p className="stat">{Math.round(weather.current.humidity)} %</p>
         </div>
+
+        </div>
+
         <div>
           <p className="stat-name">wind</p>
           <p className="stat">{Math.round(weather.current.wind_kph)} km/h</p>
         </div>
       </div>
-      <section className="hours flex">
+      <section className="hours flex auto-center">
         {hours.map((hour) => (
           <div
             className="hourly-temp-container"
             key={Number(hour.time.slice(11, 13))}
           >
             <p className="hour">{hour.time.slice(11)}</p>
-            <div className="small-celsius-container">
+            <div className="small-celsius-container flex">
               <p className="hourly-temp">{Math.round(hour.temp_c)}</p>
-              <div className="celsius-small"></div>
+              <p className="celsius-small"></p>
             </div>
           </div>
         ))}
